c183ace61a1b6a0c8ab21a6e106727fd72ef1f41
Zulfira Seitkulova <zulfira.seitkulova.kk@gmail.com>
2022-05-23 22:46:38 +0600

    Add 1st week

---
+++ script3.js
@@ -0,0 +1,5 @@
+// let userName = "Zulfira";
+// let userMale = "zulfira.seitkulova.kk@gmail.com";
+
+alert("Имя пользователя: " + userName);
+alert("Эл.Почта пользователя: " + userMale);
---
+++ script4.js
@@ -0,0 +1,9 @@
+let suvenir = 75;
+let bezdelushka = 112;
+
+let a = prompt("Введите количество сувениров");
+let b = prompt("Введите количество безделушек");
+
+let result = suvenir * a + bezdelushka * b;
+
+alert(result);
